916eb80c1ff8f5c3658b9ccc21cfdd6ca7a4eb53
Daniil <hum4noid@yandex.ru>
2023-11-04 23:27:13 +0300

    feat: Add api template

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.v1 import grades
+
 api_router = APIRouter()
+api_router.include_router(grades.router, tags=["grades"])
---
+++ app/api/v1/grades.py
@@ -0,0 +1,27 @@
+import logging
+from typing import Any, Annotated, List
+
+from fastapi.params import Depends
+from fastapi.routing import APIRouter
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from starlette.responses import Response
+
+from app.deps.db import get_async_session
+from app.schemas import GradeRead
+
+router = APIRouter(prefix="/grades")
+
+logger = logging.getLogger(__name__)
+
+SessionDB = Annotated[AsyncSession, Depends(get_async_session)]
+
+
+@router.get("", response_model=List[GradeRead])
+async def get_grades(
+        response: Response,
+        session: SessionDB,
+        skip: int = 0,
+        limit: int = 100,
+) -> Any:
+    logger.info(f"Getting grades")
+    return []
