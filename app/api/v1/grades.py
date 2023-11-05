63073efa8e7ce7af1904a8db769868a42b2acfaf
kripistor <liss01062004l@gmail.com>
2023-11-05 12:14:38 +0300

    feat: Add grade endpoints

--- app/api/v1/grades.py
+++ app/api/v1/grades.py
@@ -7,7 +7,8 @@ from sqlalchemy.ext.asyncio.session import AsyncSession
 from starlette.responses import Response
 
 from app.deps.db import get_async_session
-from app.schemas import GradeRead
+from app.repositories.grade import GradeRepo
+from app.schemas import GradeRead, GradeCreate, GradeUpdate
 
 router = APIRouter(prefix="/grades")
 
@@ -23,5 +24,31 @@ async def get_grades(
         skip: int = 0,
         limit: int = 100,
 ) -> Any:
-    logger.info(f"Getting grades")
-    return []
+    grade_repo: GradeRepo = GradeRepo(session)
+    grades = await grade_repo.get_grades()
+    if not grades:
+        logger.info(f"Grades not found")
+        return []
+    return grades
+
+
+@router.post("", response_model=GradeCreate)
+async def create_grade(
+        response: Response,
+        session: SessionDB,
+        grade: GradeCreate,
+) -> Any:
+    grade_repo: GradeRepo = GradeRepo(session)
+    await grade_repo.create_grade(grade)
+    logger.info(f"Grade created")
+
+
+@router.put("", response_model=GradeUpdate)
+async def update_grade(
+        response: Response,
+        session: SessionDB,
+        grade: GradeUpdate,
+) -> Any:
+    grade_repo: GradeRepo = GradeRepo(session)
+    await grade_repo.update_grade(grade)
+    logger.info(f"Grade updated")
