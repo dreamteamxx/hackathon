6ceb152265fb2529cba6f4de03cd98a4ffa210f6
sirok1 <s1r0k.dev@gmail.com>
2023-11-11 23:52:53 +0300

    android build files & android permissions requests

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -15,4 +15,7 @@ const store =  configureStore({
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
+export function clearStore() {
+    store.dispatch({type: "RESET"})
+}
 export default store
