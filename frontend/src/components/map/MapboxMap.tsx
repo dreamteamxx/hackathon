33292624dde069731321e55cf9a1453eb6043fde
sirok1 <s1r0k.dev@gmail.com>
2023-11-11 23:51:40 +0300

    map localization & auto geoposition

--- frontend/src/components/map/MapboxMap.tsx
+++ frontend/src/components/map/MapboxMap.tsx
@@ -1,18 +1,32 @@
-import Map from "react-map-gl"
+import Map, {GeolocateControl, useControl} from "react-map-gl"
 import 'mapbox-gl/dist/mapbox-gl.css';
+import {useRef} from "react";
+import * as mapboxgl from "mapbox-gl";
+import MapboxLanguage from "@mapbox/mapbox-gl-language";
+
 
 export default function MapboxMap(){
+    const geoControlRef = useRef<mapboxgl.GeolocateControl>(null)
+    function LanguageControl() {
+        useControl(() => new MapboxLanguage({defaultLanguage: "ru"}))
+        return null
+    }
     return (
         <Map
             mapLib={import('mapbox-gl')}
             mapboxAccessToken={import.meta.env.VITE_MAPBOX_API_TOKEN}
             initialViewState={{
-                longitude: -100,
-                latitude: 40,
+                longitude: 37.61729990,
+                latitude: 55.75582600,
                 zoom: 3.5
             }}
+            onLoad={() => {geoControlRef.current?.trigger()}}
             style={{width: "100%", height: "100vh"}}
-            mapStyle="mapbox://styles/mapbox/streets-v12"
-        />
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+
+        >
+            <GeolocateControl ref={geoControlRef}/>
+            <LanguageControl/>
+        </Map>
     )
 }
